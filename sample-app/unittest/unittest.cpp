81501d7c475626132b444cf5775a85f708e5b333
W72CT3esb <tomo-hayakawa@nec.com>
2022-09-06 10:42:23 +0900

    test CI/CD

--- sample-app/unittest/unittest.cpp
+++ sample-app/unittest/unittest.cpp
@@ -1389,4 +1389,19 @@ TEST(PerformanceTest, detect_face_elapsed_time_Test) {
 	DeleteFile(OUTPUT_FILEPATH.c_str());
 	RemoveDirectory(OUTPUT_DIRPATH.c_str());
 }
+
+// サンプルアプリ(sample-app.exe)の実行時間
+TEST(PerformanceTest, sample_app_elapsed_time_Test) {
+	std::chrono::system_clock::time_point  start, end;
+	double elapsed_time;
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	system("sample-app.exe");
+	end = std::chrono::system_clock::now();  // 計測終了時間
+
+	// 処理に要した時間をミリ秒に変換
+	elapsed_time = (double)std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+	std::cout << elapsed_time << " msec" << std::endl;
+	//ASSERT_TRUE(mean_elapsed_time <= 500); //500ms以下だったら、とりあえずOK
+}
 // ###処理時間のテストここまで###
